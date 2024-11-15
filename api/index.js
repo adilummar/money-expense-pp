eb9ca1e9ba45d39bd7685c496e3da4362c1fea6a
adilsygmetiv <adilummersygmetiv@gmail.com>
2024-11-15 10:26:36 +0530

    auth route added

--- api/index.js
+++ api/index.js
@@ -1,16 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from './routes/auth.route.js'
 
 dotenv.config();
 const app = express();
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log("mongodb connected successfuly")
-}).catch(err=>{
-    console.log(err)
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("mongodb connected successfuly");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(express.json())
+app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
 
-app.listen(3333, () => {
-  console.log("port is listening port 3333!");
+app.listen(2999, () => {
+  console.log("port is listening port 2999!");
 });
