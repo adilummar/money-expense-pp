2694b7507938522dccbd5661acb00904ed1cb2b4
adilsygmetiv <adilummersygmetiv@gmail.com>
2024-11-18 18:49:00 +0530

    sign up frontend settled

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,16 @@
-
+import {Link} from 'react-router-dom'
 export default function SignUp() {
   return (
-    <div>
-      <h1>sign up</h1>
+    <div className=" mx-auto mt-24 rounded-lg p-5 lg:w-1/2 p-24 ">
+      <h1 className="text-center mt-10 text-3xl font-bold uppercase">sign up</h1>
+      <form className="flex flex-col gap-4 mt-10 max-w-lg mx-auto">
+        <input type="text" placeholder="user name" id="username" className="third-color border-color p-3 rounded-lg text-color"/>
+        <input type="email" placeholder="email" id="email" className="third-color border-color p-3 rounded-lg text-color"/>
+        <input type="passwrod" placeholder="passwrod" id="passwrod" className="third-color border-color p-3 rounded-lg text-color"/>
+        <p>do you have an account ? <Link className='our-green' to={'/sign-in'}>sign in</Link></p>
+        <button className='our-green third-color border-color p-3 rounded-lg mt-8 uppercase font-semibold hover:opacity-75'>sign up</button>
+      </form>
+      
     </div>
   )
 }
