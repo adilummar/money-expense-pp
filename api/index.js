c65b324900617a3a11f3f89eb415f3dd42465f1c
adilsygmetiv <adilummersygmetiv@gmail.com>
2024-11-18 16:20:53 +0530

    created a middleware for handling the error

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
-import authRouter from './routes/auth.route.js'
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
@@ -15,9 +15,19 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-app.use(express.json())
-app.use('/api/user',userRouter)
-app.use('/api/auth',authRouter)
+app.use(express.json());
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(2999, () => {
   console.log("port is listening port 2999!");
