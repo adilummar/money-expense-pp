a159b22ab9d4a6c5174b7d03b8b1638e820edc35
adilsygmetiv <adilummersygmetiv@gmail.com>
2024-11-20 11:38:46 +0530

    created sign in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { signup } from '../controller/auth.controller.js'
+import { signup, singin } from '../controller/auth.controller.js'
 
 const router = express.Router()
 
 router.post('/sign-up',signup)
+router.post('/sign-in',singin)
 
 export default router
