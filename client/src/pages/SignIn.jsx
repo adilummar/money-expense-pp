aaa5fd517785c04b2a19f2c2cf6a6f7e20e4deee
adilsygmetiv <adilummersygmetiv@gmail.com>
2024-11-21 13:45:22 +0530

    added local storage with persistor

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import { signInStart, signInFaild, signInSuccess } from "../redux/user/userSlice";
 
 export default function SignIn() {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+ const {error,loading} = useSelector((state)=> state.user)
   const [formData, setFormData] = useState({});
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
-      setLoading(true);
+      dispatch(signInStart())
       const res = await fetch("/api/auth/sign-in", {
         method: "POST",
         headers: {
@@ -20,16 +22,13 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFaild(data.message))
         return;
       }
-      setLoading(false);
-      setError(null)
+      dispatch(signInSuccess(data))
       navigate('/')
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFaild(error.message))
     }
   };
 
